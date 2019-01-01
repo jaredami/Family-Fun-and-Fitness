1a7ccd8363b2a09a4f153e5e5e2d5deb7f66fa43
jaredami <jarstorm@imail.iu.edu>
2019-01-01 13:03:03 -0500

    increase fade-in on scroll speed; fix widget section for mobile

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ jQuery(document).ready(function() {
         jQuery(window).scrollTop() + jQuery(window).height();
       /* If the object is completely visible in the window, fade it it */
       if (bottom_of_window > bottom_of_object) {
-        jQuery(this).animate({ opacity: "1" }, 500);
+        jQuery(this).animate({ opacity: "1" }, 300);
       }
     });
   });
